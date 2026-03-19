e5018bb51aa96640347670c14f909e6012271481
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-19 22:41:20 +0100

    MirandaLeyvaContaoArchitectureReferencesBundle.php

---
+++ src/MirandaLeyvaContaoArchitectureReferencesBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MirandaLeyva\ContaoArchitectureReferences;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MirandaLeyvaContaoArchitectureReferencesBundle extends Bundle
+{
+}
