8c7485a1e73e0c420d9bb9a204dfadd3542a4883
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-19 23:42:37 +0100

    Frontend Modul controller erstellt

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Back end Modules
+ */
+$GLOBALS['TL_LANG']['MOD']['architecture_references'] = ['Architektur-Referenzen', 'Verwaltung der Architektur-Referenzen'];
+
+/**
+ * Front end Modules
+ */
+$GLOBALS['TL_LANG']['FMD']['references'] = 'Referenzen';
+$GLOBALS['TL_LANG']['FMD']['architecture_references_list'] = ['Architektur-Referenzen Liste', 'Gibt eine Liste der Architektur-Referenzen aus'];
+$GLOBALS['TL_LANG']['FMD']['architecture_reference_reader'] = ['Architektur-Referenz Leser', 'Gibt eine einzelne Architektur-Referenz aus'];
+
+/**
+ * tl_module
+ */
+$GLOBALS['TL_LANG']['tl_module']['reference_limit'] = ['Referenzen pro Seite', 'Anzahl der auszugebenden Referenzen'];
+$GLOBALS['TL_LANG']['tl_module']['reference_offset'] = ['Referenzen überspringen', 'Anzahl der zu überspringenden Referenzen'];
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Back end Modules
+ */
+$GLOBALS['TL_LANG']['MOD']['architecture_references'] = ['Architecture References', 'Management of architecture references'];
+
+/**
+ * Front end Modules
+ */
+$GLOBALS['TL_LANG']['FMD']['references'] = 'References';
+$GLOBALS['TL_LANG']['FMD']['architecture_references_list'] = ['Architecture References List', 'Displays a list of architecture references'];
+$GLOBALS['TL_LANG']['FMD']['architecture_reference_reader'] = ['Architecture Reference Reader', 'Displays a single architecture reference'];
+
+/**
+ * tl_module
+ */
+$GLOBALS['TL_LANG']['tl_module']['reference_limit'] = ['References per page', 'Number of references to display'];
+$GLOBALS['TL_LANG']['tl_module']['reference_offset'] = ['Skip references', 'Number of references to skip'];
