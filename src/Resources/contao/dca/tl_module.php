a1b0ac63c348ee584098a78170b2a305df24f89c
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-19 23:15:50 +0100

    tl_module.php vollständig

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,23 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['architecture_references_list']
+    = '{title_legend},name,headline,type;{config_legend},imgSize,reference_limit,reference_offset;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['architecture_reference_reader']
+    = '{title_legend},name,headline,type;{config_legend},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['reference_limit'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['reference_limit'],
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50'],
+    'sql' => "int(10) unsigned NOT NULL default 30",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['reference_offset'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['reference_offset'],
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50'],
+    'sql' => "int(10) unsigned NOT NULL default 0",
+];
