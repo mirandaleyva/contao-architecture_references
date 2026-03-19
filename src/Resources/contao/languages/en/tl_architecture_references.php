791ad76801d3517d875a324b77d7991f41c06bc8
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-19 23:25:01 +0100

    language EN fertig

---
+++ src/Resources/contao/languages/en/tl_architecture_references.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_architecture_references']['title'] = ['Title', 'Title of the reference'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['alias'] = ['Alias', 'Unique alias for the URL'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['category'] = ['Category', 'Category of the reference'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['short_description'] = ['Short description', 'Short text of the reference'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['long_description'] = ['Description', 'Detailed description'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['completion'] = ['Completion', 'Completion date or year'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['location'] = ['Location', 'Location of the project'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['work'] = ['Work', 'Type of work'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['status'] = ['Status', 'Status of the project'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['photographer'] = ['Photographer', 'Name of the photographer'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['visualizer'] = ['Visualization', 'Name of the visualization'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['photographer_link'] = ['Photographer website', 'URL to the photographer website'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['visualizer_link'] = ['Visualization website', 'URL to the visualization website'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['preview_image'] = ['Preview image', 'Image for list view'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['gallery'] = ['Gallery', 'Additional images of the reference'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['published'] = ['Publish', 'Publish the reference'];
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_architecture_references']['general_legend'] = 'General';
+$GLOBALS['TL_LANG']['tl_architecture_references']['content_legend'] = 'Content';
+$GLOBALS['TL_LANG']['tl_architecture_references']['project_legend'] = 'Project data';
+$GLOBALS['TL_LANG']['tl_architecture_references']['credits_legend'] = 'Credits';
+$GLOBALS['TL_LANG']['tl_architecture_references']['image_legend'] = 'Images';
+$GLOBALS['TL_LANG']['tl_architecture_references']['publish_legend'] = 'Publication';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_architecture_references']['new'] = ['New reference', 'Create a new reference'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['show'] = ['Details', 'Show details of reference ID %s'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['edit'] = ['Edit', 'Edit reference ID %s'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['copy'] = ['Duplicate', 'Duplicate reference ID %s'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['delete'] = ['Delete', 'Delete reference ID %s'];
+$GLOBALS['TL_LANG']['tl_architecture_references']['toggle'] = ['Toggle visibility', 'Publish or unpublish reference ID %s'];
