9238f9c87a476024c821fe13cad4524f86d2349f
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-19 22:47:12 +0100

    config.php erstellt und fertig entwickelt

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_MODELS']['tl_architecture_references']
+    = \MirandaLeyva\ContaoArchitectureReferences\Model\ArchitectureReferencesModel::class;
+
+$GLOBALS['TL_AUTO_ITEM'][] = 'architecture_reference';
