1798f3d1849e7f1bef0a3a25cd62e5cabdf9fa0e
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-03-20 13:54:30 +0100

    config.php wurde angepasst (BE_MOD hinzugefügt)

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,3 +4,7 @@ $GLOBALS['TL_MODELS']['tl_architecture_references']
     = \MirandaLeyva\ContaoArchitectureReferences\Model\ArchitectureReferencesModel::class;
 
 $GLOBALS['TL_AUTO_ITEM'][] = 'architecture_reference';
+
+$GLOBALS['BE_MOD']['content']['architecture_references'] = [
+  'tables' => ['tl_architecture_references'],
+];
