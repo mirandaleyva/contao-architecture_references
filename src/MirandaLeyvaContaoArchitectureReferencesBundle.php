64c8403e4e7994a9d58aad7d5c6e94df222cf256
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-06 22:54:59 +0200

    Bundle klasse aktualisiert, services werden geladen

--- src/MirandaLeyvaContaoArchitectureReferencesBundle.php
+++ src/MirandaLeyvaContaoArchitectureReferencesBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace MirandaLeyva\ContaoArchitectureReferences;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MirandaLeyvaContaoArchitectureReferencesBundle extends Bundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../Resources/config/services.yaml');
+    }
 }
