4f2b95a9d56684d5b0c8bd91bb877d564be170e7
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-05-19 15:18:14 +0200

    Resolve reference image UUIDs for frontend output

--- src/Controller/FrontendModule/ArchitectureReferenceReaderController.php
+++ src/Controller/FrontendModule/ArchitectureReferenceReaderController.php
@@ -5,39 +5,64 @@ namespace MirandaLeyva\ContaoArchitectureReferences\Controller\FrontendModule;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\FilesModel;
 use Contao\Input;
 use Contao\ModuleModel;
+use Contao\StringUtil;
 use MirandaLeyva\ContaoArchitectureReferences\Model\ArchitectureReferencesModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsFrontendModule(
-    category: 'references',
-    type: 'architecture_reference_reader',
-    template: 'mod_architecture_reference'
+  category: 'references',
+  type: 'architecture_reference_reader',
+  template: 'mod_architecture_reference'
 )]
 class ArchitectureReferenceReaderController extends AbstractFrontendModuleController
 {
-    protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
-    {
-        $alias = Input::get('architecture_reference') ?: Input::get('auto_item');
+  protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
+  {
+    $alias = Input::get('architecture_reference') ?: Input::get('auto_item');
 
-        if (!$alias) {
-            return new Response('', Response::HTTP_NO_CONTENT);
-        }
-
-        $reference = ArchitectureReferencesModel::findOneBy(
-            ['alias=?', 'published=?'],
-            [$alias, '1']
-        );
-
-        if (null === $reference) {
-            throw $this->createNotFoundException('Architecture reference not found.');
-        }
-
-        $template->set('reference', $reference);
-        $template->set('model', $model);
-
-        return $template->getResponse();
+    if (!$alias) {
+      return new Response('', Response::HTTP_NO_CONTENT);
     }
+
+    $reference = ArchitectureReferencesModel::findOneBy(
+      ['alias=?', 'published=?'],
+      [$alias, '1']
+    );
+
+    if (null === $reference) {
+      throw $this->createNotFoundException('Architecture reference not found.');
+    }
+
+    $previewImage = null;
+
+    if ($reference->preview_image) {
+      $file = FilesModel::findByUuid($reference->preview_image);
+
+      if (null !== $file) {
+        $previewImage = $file->path;
+      }
+    }
+
+    $galleryImages = [];
+
+    foreach (StringUtil::deserialize($reference->gallery, true) as $uuid) {
+      $file = FilesModel::findByUuid($uuid);
+
+      if (null !== $file) {
+        $galleryImages[] = $file->path;
+      }
+    }
+
+    $reference->previewImage = $previewImage;
+    $reference->galleryImages = $galleryImages;
+
+    $template->set('reference', $reference);
+    $template->set('model', $model);
+
+    return $template->getResponse();
+  }
 }
--- src/Controller/FrontendModule/ArchitectureReferencesListController.php
+++ src/Controller/FrontendModule/ArchitectureReferencesListController.php
@@ -5,6 +5,7 @@ namespace MirandaLeyva\ContaoArchitectureReferences\Controller\FrontendModule;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\FilesModel;
 use Contao\ModuleModel;
 use Contao\PageModel;
 use MirandaLeyva\ContaoArchitectureReferences\Model\ArchitectureReferencesModel;
@@ -12,47 +13,58 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsFrontendModule(
-    category: 'references',
-    type: 'architecture_references_list',
-    template: 'mod_architecture_references'
+  category: 'references',
+  type: 'architecture_references_list',
+  template: 'mod_architecture_references'
 )]
 class ArchitectureReferencesListController extends AbstractFrontendModuleController
 {
-    protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
-    {
-        $referenceModels = ArchitectureReferencesModel::findBy(
-            ['published=?'],
-            ['1'],
-            [
-                'order' => 'sorting ASC',
-                'limit' => (int) $model->reference_limit,
-                'offset' => (int) $model->reference_offset,
-            ]
-        );
+  protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
+  {
+    $referenceModels = ArchitectureReferencesModel::findBy(
+      ['published=?'],
+      ['1'],
+      [
+        'order' => 'sorting ASC',
+        'limit' => (int) $model->reference_limit,
+        'offset' => (int) $model->reference_offset,
+      ]
+    );
 
-        $references = [];
-        $jumpToPage = null;
+    $references = [];
+    $jumpToPage = null;
 
-        if ($model->jumpTo) {
-            $jumpToPage = PageModel::findByPk($model->jumpTo);
-        }
-
-        if (null !== $referenceModels) {
-            foreach ($referenceModels as $referenceModel) {
-                $references[] = [
-                    'title' => $referenceModel->title,
-                    'short_description' => $referenceModel->short_description,
-                    'alias' => $referenceModel->alias,
-                    'url' => (null !== $jumpToPage && $referenceModel->alias)
-                        ? $jumpToPage->getFrontendUrl('/' . $referenceModel->alias)
-                        : null,
-                ];
-            }
-        }
-
-        $template->set('references', $references);
-        $template->set('model', $model);
-
-        return $template->getResponse();
+    if ($model->jumpTo) {
+      $jumpToPage = PageModel::findByPk($model->jumpTo);
     }
+
+    if (null !== $referenceModels) {
+      foreach ($referenceModels as $referenceModel) {
+        $previewImage = null;
+
+        if ($referenceModel->preview_image) {
+          $file = FilesModel::findByUuid($referenceModel->preview_image);
+
+          if (null !== $file) {
+            $previewImage = $file->path;
+          }
+        }
+
+        $references[] = [
+          'title' => $referenceModel->title,
+          'short_description' => $referenceModel->short_description,
+          'alias' => $referenceModel->alias,
+          'preview_image' => $previewImage,
+          'url' => (null !== $jumpToPage && $referenceModel->alias)
+            ? $jumpToPage->getFrontendUrl('/' . $referenceModel->alias)
+            : null,
+        ];
+      }
+    }
+
+    $template->set('references', $references);
+    $template->set('model', $model);
+
+    return $template->getResponse();
+  }
 }
