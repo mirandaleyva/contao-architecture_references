3ff1d51d35a37f1f668cae93d018ca9156a044af
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-06-02 13:45:30 +0200

    .

--- src/Controller/FrontendModule/ArchitectureReferencesListController.php
+++ src/Controller/FrontendModule/ArchitectureReferencesListController.php
@@ -12,23 +12,29 @@ use MirandaLeyva\ContaoArchitectureReferences\Model\ArchitectureReferencesModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsFrontendModule(
-  category: 'references',
-  type: 'architecture_references_list',
-  template: 'mod_architecture_references'
-)]
-class ArchitectureReferencesListController extends AbstractFrontendModuleController
+#[
+  AsFrontendModule(
+    category: "references",
+    type: "architecture_references_list",
+    template: "mod_architecture_references",
+  ),
+]
+class ArchitectureReferencesListController extends
+  AbstractFrontendModuleController
 {
-  protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
-  {
+  protected function getResponse(
+    FragmentTemplate $template,
+    ModuleModel $model,
+    Request $request,
+  ): Response {
     $referenceModels = ArchitectureReferencesModel::findBy(
-      ['published=?'],
-      ['1'],
+      ["published=?"],
+      ["1"],
       [
-        'order' => 'sorting ASC',
-        'limit' => (int) $model->reference_limit,
-        'offset' => (int) $model->reference_offset,
-      ]
+        "order" => "sorting ASC",
+        "limit" => (int) $model->reference_limit,
+        "offset" => (int) $model->reference_offset,
+      ],
     );
 
     $references = [];
@@ -51,19 +57,23 @@ class ArchitectureReferencesListController extends AbstractFrontendModuleControl
         }
 
         $references[] = [
-          'title' => $referenceModel->title,
-          'short_description' => $referenceModel->short_description,
-          'alias' => $referenceModel->alias,
-          'preview_image' => $previewImage,
-          'url' => (null !== $jumpToPage && $referenceModel->alias)
-            ? $jumpToPage->getFrontendUrl('/' . $referenceModel->alias)
-            : null,
+          "id" => $referenceModel->id,
+          "title" => $referenceModel->title,
+          "short_description" => $referenceModel->short_description,
+          "alias" => $referenceModel->alias,
+          "preview_image" => $previewImage,
+          "url" =>
+            null !== $jumpToPage && $referenceModel->alias
+              ? $jumpToPage->getFrontendUrl(
+                "?project=" . $referenceModel->alias,
+              )
+              : null,
         ];
       }
     }
 
-    $template->set('references', $references);
-    $template->set('model', $model);
+    $template->set("references", $references);
+    $template->set("model", $model);
 
     return $template->getResponse();
   }
