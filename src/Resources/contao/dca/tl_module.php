b8e1ec730a5eb66b6a6024bcb2a5621bbbd58874
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-06-03 11:44:22 +0200

    tl_module.php offsetlimite fix

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,40 +1,50 @@
 <?php
 
-/**
- * Add palettes to tl_module
- */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['architecture_references_list']
-    = '{title_legend},name,headline,type;{config_legend},imgSize,reference_limit,reference_offset;{redirect_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-
-$GLOBALS['TL_DCA']['tl_module']['palettes']['architecture_reference_reader']
-    = '{title_legend},name,headline,type;{config_legend},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['reference_limit'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_module']['reference_limit'],
-    'exclude' => true,
-    'inputType' => 'text',
-    'eval' => ['rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50'],
-    'sql' => "int(10) unsigned NOT NULL default 30",
-];
-
-$GLOBALS['TL_DCA']['tl_module']['palettes']['architecture_references_list']
-    = '{title_legend},name,type;{config_legend},jumpTo;{template_legend:hide},customTpl;';
+declare(strict_types=1);
 
 /**
- * Add fields to tl_module
+ * Frontend module palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['fields']['reference_offset'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_module']['reference_offset'],
-    'exclude' => true,
-    'inputType' => 'text',
-    'eval' => ['rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50'],
-    'sql' => "int(10) unsigned NOT NULL default 0",
+$GLOBALS["TL_DCA"]["tl_module"]["palettes"]["architecture_references_list"] =
+  "{title_legend},name,headline,type;" .
+  "{config_legend},imgSize,reference_limit,reference_offset;" .
+  "{redirect_legend},jumpTo;" .
+  "{template_legend:hide},customTpl;" .
+  "{protected_legend:hide},protected;" .
+  "{expert_legend:hide},guests,cssID,space";
+
+$GLOBALS["TL_DCA"]["tl_module"]["palettes"]["architecture_reference_reader"] =
+  "{title_legend},name,headline,type;" .
+  "{config_legend},imgSize;" .
+  "{template_legend:hide},customTpl;" .
+  "{protected_legend:hide},protected;" .
+  "{expert_legend:hide},guests,cssID,space";
+
+/**
+ * Frontend module fields
+ */
+$GLOBALS["TL_DCA"]["tl_module"]["fields"]["reference_limit"] = [
+  "label" => &$GLOBALS["TL_LANG"]["tl_module"]["reference_limit"],
+  "exclude" => true,
+  "inputType" => "text",
+  "eval" => [
+    "rgxp" => "natural",
+    "mandatory" => false,
+    "tl_class" => "w50",
+    "maxlength" => 10,
+  ],
+  "sql" => "int(10) unsigned NOT NULL default 0",
 ];
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo'] = [
-    'label' => &$GLOBALS['TL_LANG']['tl_module']['jumpTo'],
-    'exclude' => true,
-    'inputType' => 'pageTree',
-    'eval' => ['mandatory'=>true, 'fieldType'=>'radio'],
-    'sql' => "int(10) unsigned NOT NULL default 0"
+$GLOBALS["TL_DCA"]["tl_module"]["fields"]["reference_offset"] = [
+  "label" => &$GLOBALS["TL_LANG"]["tl_module"]["reference_offset"],
+  "exclude" => true,
+  "inputType" => "text",
+  "eval" => [
+    "rgxp" => "natural",
+    "mandatory" => false,
+    "tl_class" => "w50",
+    "maxlength" => 10,
+  ],
+  "sql" => "int(10) unsigned NOT NULL default 0",
 ];
